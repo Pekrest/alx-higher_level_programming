7652a8c53bc9348c55bb511dfeada5fe505654de
pekrest <peteroku3@gmail.com>
2024-05-27 03:12:47 -0700

    2. Replace element task

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """
+    a function that replaces an element of a list at a specific 
+    position (like in C).
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    for x in range(len(my_list)):
+        if x == idx:
+            my_list[x] = element
+            return my_list
