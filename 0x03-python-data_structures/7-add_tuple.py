352c75d9d6bd86a5a462c67cc73a0ea8e9300a0a
pekrest <peteroku3@gmail.com>
2024-05-27 03:56:10 -0700

    7. Tuples addition task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    a function that adds 2 tuples.
+    """
+    x = tuple_x + (0, 0)
+    y = tuple_y + (0, 0)
+    result = (x[0] + y[0], x[1] + y[1])
+    return result
